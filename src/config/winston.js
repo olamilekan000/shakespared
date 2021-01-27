cf382714a701e2ad20f0536b86d4b107d59279d2
Olalekan <odukoyaonline@gmail.com>
2021-01-27 04:29:19 +0100

    initial commit

--- src/config/winston.js
+++ src/config/winston.js
@@ -2,15 +2,15 @@ const appRoot = require('app-root-path');
 const winston = require('winston');
 
 const options = {
-  file: {
-    level: 'info',
-    filename: `${appRoot}/logs/app.log`,
-    handleExceptions: true,
-    json: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-    colorize: false,
-  },
+  // file: {
+  //   level: 'info',
+  //   filename: `${appRoot}/logs/app.log`,
+  //   handleExceptions: true,
+  //   json: true,
+  //   maxsize: 5242880, // 5MB
+  //   maxFiles: 5,
+  //   colorize: false,
+  // },
   console: {
     level: 'debug',
     handleExceptions: true,
@@ -21,7 +21,7 @@ const options = {
 
 const logger = winston.createLogger({
   transports: [
-    new winston.transports.File(options.file),
+    // new winston.transports.File(options.file),
     new winston.transports.Console(options.console),
   ],
   exitOnError: false, // do not exit on handled exceptions
